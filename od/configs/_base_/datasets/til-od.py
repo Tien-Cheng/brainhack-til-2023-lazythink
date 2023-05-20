393c9d2f700c69e15c14fdbecfbd1348198a81b8
Wong Zhao Wu (Bryan) <70313929+kiritowu@users.noreply.github.com>
2023-05-21 02:09:13 +0800

    [feat] Add gaussian noise probability to match test case

--- od/configs/_base_/datasets/til-od.py
+++ od/configs/_base_/datasets/til-od.py
@@ -30,7 +30,7 @@ train_pipeline = [
     dict(type="LoadAnnotations", with_bbox=True),
     dict(type="Resize", scale=(1280, 720), keep_ratio=True),
     dict(type="RandomFlip", prob=0.5),
-    dict(type="RandomGaussian", prob=0.2),
+    dict(type="RandomGaussian", prob=0.5),
     dict(type="PackDetInputs"),
 ]
 
