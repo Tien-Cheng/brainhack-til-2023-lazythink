a49111e5c8273d0a61dbde3a72e45ea05ee4f459
Wong Zhao Wu (Bryan) <70313929+kiritowu@users.noreply.github.com>
2023-05-22 17:17:30 +0800

    [refactor] Turn reid to runnable script

--- reid/main.py
+++ reid/main.py
@@ -1,67 +1,119 @@
-import torch
-import numpy as np
-from PIL import Image
-import os
+import argparse
 import glob
+from pathlib import Path
+
+import pandas as pd
+import torch
 import open_clip
+from PIL import Image
 
-# module-level
-from src.transforms import make_classification_eval_transform
-from src.utils import load_image
-
-# constants and configs
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-SIMILARITY_THRESHOLD = 0.91
 
-# load preprocessing module and dinov2 model
-# preprocess = make_classification_eval_transform()
-# model = torch.hub.load('facebookresearch/dinov2', 'dinov2_vitl14').to(DEVICE)
-config = dict(
-    model_name='ViT-H-14',
-    pretrained='laion2b_s32b_b79k',
-#     jit=True,
-    device=DEVICE
-)
-model, preprocess = open_clip.create_model_from_pretrained(**config)
-model.eval()
-print("model loaded")
 
-# creating vectors for all suspect images
-suspect_img_path = sorted(glob.glob("data/suspects/*.png"))
-print("num image found:", len(suspect_img_path))
-with torch.inference_mode(), torch.cuda.amp.autocast():
-    suspect_preprocess = torch.stack([preprocess(load_image(x)) for x in suspect_img_path]).to(DEVICE)
-    suspect_vectors = model.encode_image(suspect_preprocess)
-    # normalize vectors prior
-    suspect_vectors /= suspect_vectors.norm(dim=-1, keepdim=True)
-    print("suspect vectors shape:", suspect_vectors.shape)
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description=(
+            "Perform cosine similarity between suspect and cropped"
+            "images using open_clip models"
+        )
+    )
+    parser.add_argument(
+        "suspect-dir",
+        help="The directory of suspect images",
+        type=str,
+    )
+    parser.add_argument(
+        "cropped-dir",
+        help="The directory of cropped images",
+        type=str,
+    )
+    parser.add_argument(
+        "output-csv",
+        help="The output csv file",
+        type=str,
+    )
+    parser.add_argument(
+        "--similarity-threshold",
+        help="The similarity threshold",
+        type=float,
+        default=0.91,
+    )
+    args = parser.parse_args()
+    return args
 
-# creating vectors for all cropped images
-cropped_img_path = sorted(glob.glob("data/test_detected/*.png"))
-for img_fp in cropped_img_path:
+
+def load_image(path: str) -> Image.Image:
+    return Image.open(path).convert("RGB")
+
+
+def load_model(config: dict):
+    print("loading model")
+    model, preprocess = open_clip.create_model_from_pretrained(**config)
+    model.eval()
+    return model, preprocess
+
+
+def encode_image(model, preprocess, img_dir: str):
+    img_paths = sorted(glob.glob(img_dir))
+    print("num image found:", len(img_paths))
     with torch.inference_mode(), torch.cuda.amp.autocast():
-        img_preprocess = preprocess(load_image(img_fp)).unsqueeze(0).to(DEVICE)
-        img_vector = model.encode_image(img_preprocess)
+        preprocesed_img = torch.stack(
+            [preprocess(load_image(x)) for x in img_paths]
+        ).to(DEVICE)
+        vectors = model.encode_image(preprocesed_img)
         # normalize vectors prior
-        img_vector /= img_vector.norm(dim=-1, keepdim=True)
-        print("img vector shape:", img_vector.shape)
+        vectors /= vectors.norm(dim=-1, keepdim=True)
+    return vectors
 
-        # compute cosine similarity
-        similarity = (img_vector @ suspect_vectors.T).squeeze(0)
-        print("similarity shape:", similarity.shape)
-        print("similarity:", similarity)
 
-        # find the most similar image
-        max_similarity = torch.max(similarity)
-        max_idx = torch.argmax(similarity)
-        print("max similarity:", max_similarity)
-        print("max idx:", max_idx)
+def main():
+    args = parse_args()
+    assert Path(args.suspect_dir).is_dir()
+    assert Path(args.cropped_dir).is_dir()
+    assert Path(args.output_csv).is_file()
 
-        # if similarity is greater than threshold, then save the image
-        if max_similarity > SIMILARITY_THRESHOLD:
-            img = Image.open(img_fp)
-            img.save(f"output/test_sus_crop/{os.path.basename(img_fp).split('.')[0]}_{os.path.basename(suspect_img_path[max_idx]).split('.')[0]}.png")
-            print("saved image")
-        else:
-            print("image not saved")
-        print()
+    suspect_dir = Path(args.suspect_dir)
+    cropped_dir = Path(args.cropped_dir)
+    output_csv = Path(args.output_csv)
+
+    # load model
+    model, preprocess = load_model(
+        dict(
+            model_name="ViT-H-14",
+            pretrained="laion2b_s32b_b79k",
+            #     jit=True,
+            device=DEVICE,
+        )
+    )
+
+    # encode suspect images
+    suspect_vectors = encode_image(
+        model, preprocess, str(suspect_dir / "*.png")
+    )
+
+    # encode cropped images
+    cropped_vectors = encode_image(
+        model, preprocess, str(cropped_dir / "*.png")
+    )
+
+    # compute cosine similarity
+    # shape=(num_cropped, num_suspect)
+    similarity_matrix = (cropped_vectors @ suspect_vectors.T).squeeze(0)
+
+    # find the most similar image
+    max_similarity = torch.max(similarity_matrix, dim=1)
+
+    # check if similarity is greater than threshold
+    is_suspect = torch.where(max_similarity > args.similarity_threshold)
+
+    # save to csv
+    pd.DataFrame(
+        dict(
+            suspect_file_name=list(suspect_dir.glob("*.png")),
+            is_suspect=is_suspect.tolist(),
+        )
+    ).to_csv(output_csv, index=False)
+
+
+if __name__ == "__main__":
+    main()
--- reid/src/utils.py
+++
@@ -1,4 +0,0 @@
-from PIL import Image
-
-def load_image(path: str) -> Image.Image:
-    return Image.open(path).convert("RGB")
