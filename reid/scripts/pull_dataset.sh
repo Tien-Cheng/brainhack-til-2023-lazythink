34fdc18b0e41018031bb8c3acc7c82ce6de61e4a
Wong Zhao Wu (Bryan) <70313929+kiritowu@users.noreply.github.com>
2023-05-25 23:50:53 +0800

    [feat] Script to pull dataset from gcs

---
+++ reid/scripts/pull_dataset.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Authenticate to GCP
+gcloud auth login
+
+# Pull the dataset from GCS
+mkdir -p /tmp/cv
+gsutil cp gs://cloud-ai-platform-e8edc327-855c-4911-bb8e-205517f8c899/cv/Train.zip /tmp/Train.zip
+gsutil cp gs://cloud-ai-platform-e8edc327-855c-4911-bb8e-205517f8c899/cv/Validation.zip /tmp/Validation.zip
+gsutil cp gs://cloud-ai-platform-e8edc327-855c-4911-bb8e-205517f8c899/cv/Test.zip /tmp/Test.zip
+gsutil cp gs://cloud-ai-platform-e8edc327-855c-4911-bb8e-205517f8c899/cv/train_labels.zip /tmp/train_labels.zip
+gsutil cp gs://cloud-ai-platform-e8edc327-855c-4911-bb8e-205517f8c899/cv/val_labels.zip /tmp/val_labels.zip
+
+# Unzip the dataset
+mkdir -p data/images
+mkdir -p data/labels/yolo
+unzip -q -o /tmp/cv/Train.zip -d data/images/train
+unzip -q -o /tmp/cv/Validation.zip -d data/images/validation
+unzip -q -o /tmp/cv/Test.zip -d data/images/test
+
+unzip -q -o /tmp/cv/train_labels.zip -d data/labels/yolo/train_labels
+unzip -q -o /tmp/cv/val_labels.zip -d data/labels/yolo/val_labels
