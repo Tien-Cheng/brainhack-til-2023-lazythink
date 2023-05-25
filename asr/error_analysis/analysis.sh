293d91f04177288f84c7337d6a282a89653bf3b3
Rachel Tan <racheltanmz@gmail.com>
2023-05-25 23:35:27 +0800

    Added error analysis files

---
+++ asr/error_analysis/analysis.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Must download the audio files in the train set to get the waveform and spectrogram
+# Change file location accordingly
+python data_explorer.py error_val_manifest.json -a
